d2b6c9b420bdc4743aa60ba0878ffd8b8fa11f82
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-04-08 14:59:13 -0400

    fix using `max - min` instead of average to get middle of bounds

--- firestarr/scripts/update.sh
+++ firestarr/scripts/update.sh
@@ -8,8 +8,8 @@ if [ -z "${BOUNDS_LATITUDE_MAX}" ] \
     ; then
     echo Bounds must be set
 else
-    LATITUDE=$((${BOUNDS_LATITUDE_MAX} - ${BOUNDS_LATITUDE_MIN}))
-    LONGITUDE=$((${BOUNDS_LONGITUDE_MAX} - ${BOUNDS_LONGITUDE_MIN}))
+    LATITUDE=$(($((${BOUNDS_LATITUDE_MAX} + ${BOUNDS_LATITUDE_MIN})) / 2))
+    LONGITUDE=$(($((${BOUNDS_LONGITUDE_MAX} + ${BOUNDS_LONGITUDE_MIN})) / 2))
     MODEL=geps
     # specifying member doesn't seem to hurt for other models
     URL_TEST="https://app-cwfmf-api-cwfis-dev.wittyplant-59b495b3.canadacentral.azurecontainerapps.io/gribwx?lat=${LATITUDE}&lon=${LONGITUDE}&model=${MODEL}&timezone=UTC&duration=1&format=csv&precision=1&latest=True&member=1"
