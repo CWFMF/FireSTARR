1962b1d3a1830002aed2900344c2e50aeae82aba
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-06-19 13:26:37 -0400

    fix broken force_run.sh not loading venv

--- firestarr/scripts/force_run.sh
+++ firestarr/scripts/force_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIR=`dirname $(realpath "$0")`
-export FORCE_RUN=1
-export IS_CRONJOB=${IS_CRONJOB}
+. /appl/data/config || . /appl/config
+source /appl/.venv/bin/activate || echo No venv
+cd /appl/firestarr
 ${DIR}/with_lock_update.sh python /appl/firestarr/src/py/firestarr/main.py $*
