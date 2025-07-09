aaa5a4dcadbfcd510e2fd22d2dd592cecbaf42ce
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-07-09 15:02:20 -0400

    don't override config variables in publish_geoserver.sh

--- firestarr/scripts/publish_geoserver.sh
+++ firestarr/scripts/publish_geoserver.sh
@@ -17,37 +17,37 @@ else
         RUN_ID=`ls -1 /appl/data/sims/ | tail -1`
     fi
     # RESOURCE_PREFIX is empty or 'test_'
-    GEOSERVER_COVERAGE="${RESOURCE_PREFIX}${GEOSERVER_COVERAGE}"
-    AZURE_DIR_DATA="${RESOURCE_PREFIX}${AZURE_DIR_DATA}"
+    COVERAGE="${RESOURCE_PREFIX}${GEOSERVER_COVERAGE}"
+    DIR_DATA="${RESOURCE_PREFIX}${AZURE_DIR_DATA}"
 
-    GEOSERVER_DIR_DATA="${GEOSERVER_DIR_ROOT}/${AZURE_DIR_DATA}/${RUN_ID}"
+    GEOSERVER_DIR_DATA="${GEOSERVER_DIR_ROOT}/${DIR_DATA}/${RUN_ID}"
     if [ -z "${TMPDIR}" ]; then
         TMPDIR=/tmp
     fi
     GEOSERVER_EXTENSION=imagemosaic
     GEOSERVER_WORKSPACE=${GEOSERVER_SERVER}/workspaces/${GEOSERVER_WORKSPACE_NAME}
-    GEOSERVER_STORE=${GEOSERVER_WORKSPACE}/coveragestores/${GEOSERVER_COVERAGE}
-    TMP_COVERAGE=${TMPDIR}/${GEOSERVER_COVERAGE}.xml
+    GEOSERVER_STORE=${GEOSERVER_WORKSPACE}/coveragestores/${COVERAGE}
+    TMP_COVERAGE=${TMPDIR}/${COVERAGE}.xml
     TAG=abstract
     echo "Publishing to ${GEOSERVER_STORE}"
 
     # get rid of old granules
-    curl -v -v -sS -u "${GEOSERVER_CREDENTIALS}" -XDELETE "${GEOSERVER_STORE}/coverages/${GEOSERVER_COVERAGE}/index/granules.xml"
+    curl -v -v -sS -u "${GEOSERVER_CREDENTIALS}" -XDELETE "${GEOSERVER_STORE}/coverages/${COVERAGE}/index/granules.xml"
     # update to match azure mount
     curl -v -u "${GEOSERVER_CREDENTIALS}" -XPOST -H "Content-type: text/plain" --write-out %{http_code} -d "${GEOSERVER_DIR_DATA}" "${GEOSERVER_STORE}/external.${GEOSERVER_EXTENSION}"
 
     # get run id from name of files
-    RUN_ID=`curl -v -v -sS -u "${GEOSERVER_CREDENTIALS}" -XGET "${GEOSERVER_STORE}/coverages/${GEOSERVER_COVERAGE}/index/granules.xml" | grep .tif | tail -n 1 | sed "s/.*firestarr_\([0-9]*\)_.*\.tif.*/\1/g"`
+    RUN_ID=`curl -v -v -sS -u "${GEOSERVER_CREDENTIALS}" -XGET "${GEOSERVER_STORE}/coverages/${COVERAGE}/index/granules.xml" | grep .tif | tail -n 1 | sed "s/.*firestarr_\([0-9]*\)_.*\.tif.*/\1/g"`
     ABSTRACT="FireSTARR run from ${RUN_ID}"
     # replace tag
-    curl -v -v -sS -u "${GEOSERVER_CREDENTIALS}" -XGET "${GEOSERVER_STORE}/coverages/${GEOSERVER_COVERAGE}" > ${TMP_COVERAGE}
+    curl -v -v -sS -u "${GEOSERVER_CREDENTIALS}" -XGET "${GEOSERVER_STORE}/coverages/${COVERAGE}" > ${TMP_COVERAGE}
     TAG_UPDATED="<${TAG}>${ABSTRACT}<\/${TAG}>"
     # if no tag then insert it after title
     (grep "<${TAG}>" ${TMP_COVERAGE} > /dev/null && sed -i "s/<${TAG}>[^<]*<\/${TAG}>/${TAG_UPDATED}/g" ${TMP_COVERAGE}) || sed -i "s/\( *\)\(<title>.*\)/\1\2\n\1${TAG_UPDATED}/g" ${TMP_COVERAGE}
     # upload with updated tag
-    curl -v -u "${GEOSERVER_CREDENTIALS}" -XPUT -H "Content-type: text/xml" -d @${TMP_COVERAGE} "${GEOSERVER_STORE}/coverages/${GEOSERVER_COVERAGE}"?calculate=nativebbox,latlonbbox,dimensions
+    curl -v -u "${GEOSERVER_CREDENTIALS}" -XPUT -H "Content-type: text/xml" -d @${TMP_COVERAGE} "${GEOSERVER_STORE}/coverages/${COVERAGE}"?calculate=nativebbox,latlonbbox,dimensions
     # not sure why this isn't picking up .tif band description
     sed -i "s/GRAY_INDEX/probability/g" ${TMP_COVERAGE}
     # HACK: calculate sets band name to GRAY_INDEX so set again without calculate
-    curl -v -u "${GEOSERVER_CREDENTIALS}" -XPUT -H "Content-type: text/xml" -d @${TMP_COVERAGE} "${GEOSERVER_STORE}/coverages/${GEOSERVER_COVERAGE}"
+    curl -v -u "${GEOSERVER_CREDENTIALS}" -XPUT -H "Content-type: text/xml" -d @${TMP_COVERAGE} "${GEOSERVER_STORE}/coverages/${COVERAGE}"
 fi
