613fbe9ffb89a2f1d869a87950112cc91415a926
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-07-09 13:57:55 -0400

    call publish_geoserver.sh properly

--- firestarr/src/py/firestarr/publish_geoserver.py
+++ firestarr/src/py/firestarr/publish_geoserver.py
@@ -6,7 +6,7 @@ from common import DIR_SCRIPTS, logging, run_process
 # HACK: just call script for now
 def publish_folder(dir_output):
     run_id = os.path.basename(dir_output)
-    stdout, stderr = run_process(["/bin/bash", "-c", f"'./publish_geoserver.sh {run_id}'"], DIR_SCRIPTS)
+    stdout, stderr = run_process(["/bin/bash", "-c", f"./publish_geoserver.sh {run_id}"], DIR_SCRIPTS)
     logging.info(stdout)
     logging.error(stderr)
     return True
