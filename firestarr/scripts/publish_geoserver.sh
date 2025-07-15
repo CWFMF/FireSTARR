9657a7b5b5a315b6591d75538b17030987f4ec6d
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-07-15 06:54:51 -0400

    fix LAYER vs COVERAGE name in publish

--- firestarr/scripts/publish_geoserver.sh
+++ firestarr/scripts/publish_geoserver.sh
@@ -34,13 +34,13 @@ else
     echo "Publishing to ${GEOSERVER_STORE}"
 
     # HACK: get rid of granules for interim fires in case they've finished and the files no longer exist
-    curl -v -v -sS -u "${GEOSERVER_CREDENTIALS}" -XDELETE "${GEOSERVER_STORE}/coverages/${COVERAGE}/index/granules.xml?filter=location%20like%27%__tmp__%%27"
+    curl -v -v -sS -u "${GEOSERVER_CREDENTIALS}" -XDELETE "${GEOSERVER_STORE}/coverages/${LAYER}/index/granules.xml?filter=location%20like%27%__tmp__%%27"
 
     # update to match azure mount
     curl -v -u "${GEOSERVER_CREDENTIALS}" -XPOST -H "Content-type: text/plain" --write-out %{http_code} -d "${GEOSERVER_DIR_DATA}" "${GEOSERVER_STORE}/external.${GEOSERVER_EXTENSION}"
 
     # get rid of old granules
-    curl -v -v -sS -u "${GEOSERVER_CREDENTIALS}" -XDELETE "${GEOSERVER_STORE}/coverages/${COVERAGE}/index/granules.xml?filter=location%20not%20like%27%${RUN_ID}%%27"
+    curl -v -v -sS -u "${GEOSERVER_CREDENTIALS}" -XDELETE "${GEOSERVER_STORE}/coverages/${LAYER}/index/granules.xml?filter=location%20not%20like%27%${RUN_ID}%%27"
 
     # extract timestamp from RUN_ID
     RUN_TIME=`echo ${RUN_ID} | sed "s/.*_\([0-9]*\).*/\1/g"`
