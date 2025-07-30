819cf4bf96f55c6dad1af2c321c294eab25e9e9d
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-07-30 12:41:13 -0400

    move cpp code into submodule

--- firestarr/src/cpp/LogPoints.cpp
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) His Majesty the King in Right of Canada as represented by the Minister of Natural Resources, 2024-2025. */
-
-/* SPDX-License-Identifier: AGPL-3.0-or-later */
-
-#include "LogPoints.h"
-
-namespace fs::sim
-{
-}
