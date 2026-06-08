574e30622e5409b7cac29d06617fd925d2ca1775
download.docker.com <jordan.evens@NRCan-RNCan.gc.ca>
2026-06-08 08:37:04 -0400

    change tag.sh to push to :dev

--- tag.sh
+++ tag.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 set -e
-pushd firestarr
 VERSION=`sed -n "/^VERSION/{s/.*=\(.*\)/\1/g;p;}" .env`
-SRCS=(firestarr)
+SRCS=(firestarr-app)
 # REPOS=(ghcr.io/jordan-evens/ registrycwfisdev.azurecr.io/firestarr/)
-REPOS=(ghcr.io/jordan-evens/)
+# REPOS=(ghcr.io/jordan-evens/)
+REPOS=(registrycwfisdev.azurecr.io/firestarr/)
+BRANCH="dev"
+# BRANCH="latest"
 
 echo ${GHCR_TOKEN}  | docker login ghcr.io -u jordan-evens --password-stdin
-# az acr login --name registrycwfisdev || (az login && az acr login --name registrycwfisdev)
+az acr login --name registrycwfisdev || (az login && az acr login --name registrycwfisdev)
 build_tag_and_push() {
     echo $1
     echo $2
     src=$1
     img="${src}:${VERSION}"
     repo=$2
-    docker compose build ${src}
+    docker compose build ${src}-hotfix
     set +e
     docker rmi ${repo}${img}
-    docker rmi ${repo}${src}:latest
+    docker rmi ${repo}${src}:${BRANCH}
     set -e
     docker tag ${img} ${repo}${img}
-    docker tag ${img} ${repo}${src}:latest
+    docker tag ${img} ${repo}${src}:${BRANCH}
     docker push ${repo}${src}
     docker push ${repo}${img}
-    docker push ${repo}${src}:latest
+    docker push ${repo}${src}:${BRANCH}
 }
 
 for src in ${SRCS[*]}; do
@@ -31,5 +33,3 @@ for src in ${SRCS[*]}; do
         build_tag_and_push ${src} ${repo}
     done
 done
-
-popd
